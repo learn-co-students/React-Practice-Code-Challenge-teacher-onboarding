0d24bf74eb98e5bfe6bff3fa8abc83707a1d1751
Levi Miller <paullevimiller@gmail.com>
2019-10-03 17:34:09 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,15 +3,64 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  state = {
+    money: 100,
+    sushi: [],
+    sushiIndex: 0
+  };
+
+  componentDidMount() {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(json => this.setState({sushi: json}))
+    this.setState({
+      sushi: this.state.sushi.map(sushi => {return {...sushi, eaten: false}})
+    });
+  };
+
+  handleClickEatSushi = (eatenSushi) => {
+    if (this.state.money > eatenSushi.price) {
+      let sushiArray = this.state.sushi.map(sushi => {
+        if (sushi.id === eatenSushi.id) {
+          sushi.eaten = true
+          return sushi
+        } else {
+          return sushi
+        }})
+      this.setState({
+        money: this.state.money - eatenSushi.price,
+        sushi: sushiArray
+      });
+    } else {
+      return;
+    };
+  };
+
+  handleClickMore = () => {
+    this.setState({
+      sushiIndex: this.state.sushiIndex + 4
+    });
+  };
+
+  getSushi = () => {
+    let sushiArray = this.state.sushi.slice(this.state.sushiIndex, this.state.sushiIndex + 4);
+    return sushiArray;
+  };
+
+  getEmptyPlates = () => this.state.sushi.filter(sushi => sushi.eaten);
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer 
+          sushi={this.getSushi()} 
+          handleClickEatSushi={this.handleClickEatSushi}
+          handleClickMore={this.handleClickMore}
+        />
+        <Table moneyLeft={this.state.money} plates={this.getEmptyPlates()} />
       </div>
     );
   }
